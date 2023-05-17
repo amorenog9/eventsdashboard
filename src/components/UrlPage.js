8eb01a7e3f5277b49eec204042f102eeedac10a7
amorenog9 <alexmgtrb@gmail.com>
2023-05-18 01:25:12 +0200

    kafka ui

--- src/components/UrlPage.js
+++ src/components/UrlPage.js
@@ -1,3 +1,4 @@
+import kafka_logo from '../images/kafka_logo.png'
 import spark_logo from '../images/spark_logo.png'
 import flink_logo from '../images/flink_logo.png'
 import './../css/UrlPage.css';
@@ -6,7 +7,15 @@ import './../css/UrlPage.css';
 function UrlPage(props) {
 
   return (
-    <div class="container">
+    <div class="containerURL">
+
+      <div class="kafka">
+        <img src={kafka_logo} alt="Kafka" />
+        <div class="link_kafka">
+          <a href="http://localhost:9080/">Interfaz visual de Apache Kafka</a>
+        </div>
+      </div>
+
       <div class="flink">
         <img src={flink_logo} alt="Flink" />
         <div class="link_flink">
@@ -19,6 +28,8 @@ function UrlPage(props) {
           <a href="http://localhost:8080/">Cluster para visualizar los jobs de spark</a>
         </div>
       </div>
+
+
     </div>
   );
 }
